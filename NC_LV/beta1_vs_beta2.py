db7d177aa1893a2cc1129049c2f2200d98005c20
crheller <charlieheller95@gmail.com>
2020-05-26 21:48:34 -0700

    add exploratly lv analysis script

---
+++ NC_LV/beta1_vs_beta2.py
@@ -0,0 +1,6 @@
+"""
+Compare beta1 and beta2 weights.
+    beta1 measured as mean big minus mean small FR axis
+    beta2 meausred as PC1 of cov(small) - cov(big)
+    * see noise_correlations/example_site_LV.py
+"""
