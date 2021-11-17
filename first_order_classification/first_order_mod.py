b93cdee5bd68e56db4e4f3cfbef9046d45235eae
crheller <charlieheller95@gmail.com>
2021-11-17 00:04:00 -0800

    cache pupil stats, work on loading first order models

--- first_order_classification/first_order_mod.py
+++ first_order_classification/first_order_mod.py
@@ -1,3 +1,22 @@
 """
 quanitfy / cache a summary of first order pupil mod for each site
+so, load model fits and compare shuffled vs. unshuffled. Use jackknifed results
 """
+from nems.xform_helper import load_model_xform
+from nems_lbhb.baphy_io import parse_cellid
+from global_settings import CPN_SITES, HIGHR_SITES
+
+m = 'psth.fs4.pup-ld-st.pup-epcpn-hrc-psthfr.z_stategain.SxR_jk.nf10-tfinit.n.lr1e4.cont.et5.i50000'
+m0 = 'psth.fs4.pup-ld-st.pup0-epcpn-hrc-psthfr.z_stategain.SxR_jk.nf10-tfinit.n.lr1e4.cont.et5.i50000'
+
+sites = CPN_SITES + HIGHR_SITES
+batches = [331] * len(CPN_SITES) + [322] * len(HIGHR_SITES)
+
+for i, (site, batch) in enumerate(zip(sites, batches)):
+    if site in ["BOL005c", "BOL006b"]:
+        batch = 294
+    if batch != 331:
+        _m = m.replace("-epcpn", "")
+        _m0 = m.replace("-epcpn", "")
+    cellist, options = parse_cellid({"cellid": site, "batch": batch})
+    xf, ctx = load_model_xform(cellid=cellist[0], batch=batch, modelname=_m)
--- first_order_classification/pupil_stats.py
+++ first_order_classification/pupil_stats.py
@@ -1,3 +1,34 @@
 """
-quanitfy / cache a summary of pupil variability / mean for each site
+quantify / cache a summary of pupil variability / mean for each site
 """
+import charlieTools.nat_sounds_ms.decoding as decoding
+from global_settings import CPN_SITES, HIGHR_SITES
+
+import numpy as np
+import pandas as pd
+
+saveto = "/auto/users/hellerc/code/projects/nat_pupil_ms/first_order_classification/pupil_stats.pickle"
+
+sites = CPN_SITES + HIGHR_SITES
+batches = [331] * len(CPN_SITES) + [322] * len(HIGHR_SITES)
+
+df = pd.DataFrame(index=np.arange(len(sites)), columns=["site", "batch", "p_std", "p_median", "p_mean", "p_norm_std", "p_norm_median", "p_norm_mean"])
+
+for i, (site, batch) in enumerate(zip(sites, batches)):
+    if site in ['BOL005c', 'BOL006b']:
+        batch = 294
+    X, sp_bins, X_pup, pup_mask = decoding.load_site(site=site, batch=batch)
+    p = X_pup.flatten()
+    max_pupil = decoding.get_max_pupil(site[:7], rasterfs=4)
+    df.loc[i, :] = [
+        site,
+        batch,
+        p.std(),
+        np.median(p),
+        p.mean(),
+        (p / max_pupil).std(),
+        np.median(p / max_pupil),
+        (p / max_pupil).mean()
+    ]
+
+df.to_pickle(saveto)
