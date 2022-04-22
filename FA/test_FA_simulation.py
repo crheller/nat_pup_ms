71235dabda24f47ce70e0407febbcdf5f2f2cb31
crheller <charlieheller95@gmail.com>
2022-04-21 22:48:16 -0700

    conirm FA simulation is doing what we want to correlations

--- FA/test_FA_simulation.py
+++ FA/test_FA_simulation.py
@@ -29,6 +29,7 @@ Xsim2, psim2 = decoding.load_FA_model(site, batch, big_psth, small_psth, sim=2, 
 Xsim3, psim3 = decoding.load_FA_model(site, batch, big_psth, small_psth, sim=3, nreps=10000)
 Xsim4, psim4 = decoding.load_FA_model(site, batch, big_psth, small_psth, sim=4, nreps=10000)
 Xsim5, psim5 = decoding.load_FA_model(site, batch, big_psth, small_psth, sim=5, nreps=10000)
+Xsim6, psim6 = decoding.load_FA_model(site, batch, big_psth, small_psth, sim=6, nreps=10000)
 
 pair = (27, 30)
 stim = (1, 2)
@@ -56,59 +57,109 @@ for i, Xs in zip(range(4), [Xsim1, Xsim2, Xsim3, Xsim4]):
 f.tight_layout()
 
 # evaluate over all neurons given stimulus
-f, ax = plt.subplots(1, 5, figsize=(20, 4), sharex=True, sharey=True)
+
+# single neuron variance
+f, ax = plt.subplots(1, 6, figsize=(18, 3), sharex=True, sharey=True)
+
+bb = np.var(Xsim6[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
+s1 = np.var(Xsim6[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[0].scatter(bb, s1, s=25)
+
 bb = np.var(Xsim1[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
 s1 = np.var(Xsim1[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
-ax[0].scatter(bb, s1, s=25)
-s2 = np.var(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
-ax[1].scatter(bb, s2, s=25)
-s3 = np.var(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
-ax[2].scatter(bb, s3, s=25)
-s4 = np.var(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
-ax[3].scatter(bb, s4, s=25)
-s5 = np.var(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[1].scatter(bb, s1, s=25)
+
 bb = np.var(Xsim5[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
-ax[4].scatter(bb, s5, s=25)
+s1 = np.var(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[2].scatter(bb, s1, s=25)
+
+bb = np.var(Xsim3[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
+s1 = np.var(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[3].scatter(bb, s1, s=25)
+
+bb = np.var(Xsim2[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
+s1 = np.var(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[4].scatter(bb, s1, s=25)
+
+bb = np.var(Xsim4[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]], axis=1)
+s1 = np.var(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]], axis=1)
+ax[5].scatter(bb, s1, s=25)
 for a in ax:
     a.plot([0, 15], [0, 15], "k--")
+    a.set_xlabel("Large pupil")
+    a.set_ylabel("Small pupil")
 f.suptitle("Single neuron variance")
 
 # evaluate over all neurons given stimulus cc
-f, ax = plt.subplots(1, 5, figsize=(20, 4), sharex=True, sharey=True)
+f, ax = plt.subplots(1, 6, figsize=(18, 3), sharex=True, sharey=True)
+
+bb = np.corrcoef(Xsim6[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.corrcoef(Xsim6[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[0].scatter(bb, s1, s=5)
+
 bb = np.corrcoef(Xsim1[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
 s1 = np.corrcoef(Xsim1[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[0].scatter(bb, s1, s=5)
-s2 = np.corrcoef(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[1].scatter(bb, s2, s=5)
-s3 = np.corrcoef(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[2].scatter(bb, s3, s=5)
-s4 = np.corrcoef(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[3].scatter(bb, s4, s=5)
-s5 = np.corrcoef(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[1].scatter(bb, s1, s=5)
+
 bb = np.corrcoef(Xsim5[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[4].scatter(bb, s5, s=5)
+s1 = np.corrcoef(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[2].scatter(bb, s1, s=5)
+
+bb = np.corrcoef(Xsim3[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.corrcoef(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[3].scatter(bb, s1, s=5)
+
+bb = np.corrcoef(Xsim2[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.corrcoef(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[4].scatter(bb, s1, s=5)
+
+bb = np.corrcoef(Xsim4[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.corrcoef(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[5].scatter(bb, s1, s=5)
+
+ll = np.min(ax[0].get_xlim() + ax[0].get_ylim())
+ul = np.max(ax[0].get_xlim() + ax[0].get_ylim())
 for a in ax:
-    a.plot([-.2, 0.5], [-.2, 0.5], "k--")
-    a.set_xlim((-.2, 0.5))
-    a.set_ylim((-.2, 0.5))
+    a.plot([ll, ul], [ll, ul], "k--")
+    # a.set_xlim((-.2, 0.5))
+    # a.set_ylim((-.2, 0.5))
+    a.set_xlabel("Big pupil")
+    a.set_ylabel("Small pupil")
 f.suptitle("correlation coefficient")
 
 
-f, ax = plt.subplots(1, 5, figsize=(20, 4), sharex=True, sharey=True)
+f, ax = plt.subplots(1, 6, figsize=(18, 3), sharex=True, sharey=True)
+
+bb = np.cov(Xsim6[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.cov(Xsim6[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[0].scatter(bb, s1, s=5)
+
 bb = np.cov(Xsim1[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
 s1 = np.cov(Xsim1[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[0].scatter(bb, s1, s=5)
-s2 = np.cov(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[1].scatter(bb, s2, s=5)
-s3 = np.cov(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[2].scatter(bb, s3, s=5)
-s4 = np.cov(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[3].scatter(bb, s4, s=5)
-s5 = np.cov(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[1].scatter(bb, s1, s=5)
+
 bb = np.cov(Xsim5[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
-ax[4].scatter(bb, s5, s=5)
+s1 = np.cov(Xsim5[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[2].scatter(bb, s1, s=5)
+
+bb = np.cov(Xsim3[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.cov(Xsim3[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[3].scatter(bb, s1, s=5)
+
+bb = np.cov(Xsim2[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.cov(Xsim2[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[4].scatter(bb, s1, s=5)
+
+bb = np.cov(Xsim4[:, psim1[0, :, 0, 0]==True, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+s1 = np.cov(Xsim4[:, psim1[0, :, 0, 0]==False, stim[0], stim[1]])[np.triu_indices(X.shape[0], 1)]
+ax[5].scatter(bb, s1, s=5)
+
+ll = np.min(ax[0].get_xlim() + ax[0].get_ylim())
+ul = np.max(ax[0].get_xlim() + ax[0].get_ylim())
 for a in ax:
-    a.plot([-1, 2], [-1, 2], "k--")
-    a.set_xlim((-1, 2))
-    a.set_ylim((-1, 2))
-f.suptitle("raw covariance")
+    a.plot([ll, ul], [ll, ul], "k--")
+    # a.set_xlim((-.2, 0.5))
+    # a.set_ylim((-.2, 0.5))
+    a.set_xlabel("Big pupil")
+    a.set_ylabel("Small pupil")
+f.suptitle("Covariance")
